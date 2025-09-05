f677672e24e8c1403d10774ab9bcc6fd65602735
k.savostin <k.savostin@inventos.ru>
2025-09-05 21:09:48 +0300

    chore: update setup file

--- setup.py
+++ setup.py
@@ -1,78 +1,21 @@
-import io
-import os
-import re
-import sys
-
 import setuptools
-
-VERSION = "0.0.1"
-
-
-def parse_requirements(fname="requirements.txt", with_version=True):
-    require_fpath = fname
-
-    def parse_line(line):
-        """Parse information from a line in a requirements text file."""
-        if line.startswith("-r "):
-            # Allow specifying requirements in other files
-            target = line.split(" ")[1]
-            for info in parse_require_file(target):
-                yield info
-        else:
-            info = {"line": line}
-            if line.startswith("-e "):
-                info["package"] = line.split("#egg=")[1]
-            elif "@git+" in line:
-                info["package"] = line
-            else:
-                # Remove versioning from the package
-                pat = "(" + "|".join([">=", "==", ">"]) + ")"
-                parts = re.split(pat, line, maxsplit=1)
-                parts = [p.strip() for p in parts]
-
-                info["package"] = parts[0]
-                if len(parts) > 1:
-                    version_operator, rest = parts[1:]
-                    if ";" in rest:
-                        # Handle platform specific dependencies
-                        # http://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-platform-specific-dependencies
-                        version, platform_deps = map(str.strip, rest.split(";"))
-                        info["platform_deps"] = platform_deps
-                    else:
-                        version = rest  # NOQA
-                    info["version"] = (version_operator, version)
-            yield info
-
-    def parse_require_file(fpath):
-        with open(fpath, "r", encoding="utf-8") as file:
-            for line in file.readlines():
-                line = line.strip()
-                if line and not line.startswith("#"):
-                    for info in parse_line(line):
-                        yield info
-
-    def gen_packages_items():
-        if os.path.exists(require_fpath):
-            for info in parse_require_file(require_fpath):
-                parts = [info["package"]]
-                if with_version and "version" in info:
-                    parts.extend(info["version"])
-                if not sys.version.startswith("3.4"):
-                    # apparently package_deps are broken in 3.4
-                    platform_deps = info.get("platform_deps")
-                    if platform_deps is not None:
-                        parts.append(";" + platform_deps)
-                item = "".join(parts)
-                yield item
-
-    packages = list(gen_packages_items())
-    return packages
-
 
 setuptools.setup(
     name="GDER-Agent",
-    version=VERSION,
+    version="0.0.1",
     packages=setuptools.find_packages(),
-    install_requires=parse_requirements(),
+    install_requires=[
+        "beautifulsoup4==4.13.5",
+        "fastapi==0.116.1",
+        "google-api-python-client==2.181.0",
+        "google-auth-httplib2==0.2.0",
+        "google-auth-oauthlib==1.2.2",
+        "python-dotenv==1.1.1",
+        "langchain-openai==0.3.32",
+        "langgraph==0.6.6",
+        "openpyxl==3.1.5",
+        "pandas==2.3.2"
+        "uvicorn==0.35.0"
+    ],
     python_requires=">=3.11",
 )
