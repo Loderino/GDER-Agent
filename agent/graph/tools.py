cb711abf38a71002cbb39e044ef16c927f3c551c
k.savostin <k.savostin@inventos.ru>
2025-09-05 19:30:38 +0300

    feat: add tools for excel file reading

--- agent/graph/tools.py
+++ agent/graph/tools.py
@@ -1,14 +1,22 @@
 from langchain_core.tools import StructuredTool
 
+from agent.excel.readers_manager import Manager
 from agent.parser.parser import WebParser
 
 parser = WebParser()
 
-def get_tools() -> list[StructuredTool]:
+def get_tools(user_id: str) -> list[StructuredTool]:
     """
-    Returns a list of tools functions for llm.
+    Returns a list of tools functions for llm, specified for user.
     """
     tools = []
-    for func in [parser.get_site_info]:
+    excel_reader = Manager.get_reader(user_id)
+    for func in [
+        parser.get_site_info,
+        excel_reader.get_sheet_preview,
+        excel_reader.search_data,
+        excel_reader.get_cell_value,
+        excel_reader.analyze_column
+    ]:
         tools.append(StructuredTool.from_function(func, name=func.__name__, description=func.__doc__))
     return tools
